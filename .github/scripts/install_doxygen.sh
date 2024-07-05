f25bfbed495b663be1963b2f0eb86cb545f9045f
Gillian Petro <gillian.petro@noaa.gov>
2024-07-05 10:22:26 -0400

    clean up doxygen install script

--- .github/scripts/install_doxygen.sh
+++ .github/scripts/install_doxygen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Steps to install Doxygen
-# Doxygen prerequisites flex, bison, make are already installed on Ubuntu-22.04 free runner
 
 sudo apt-get install doxygen
 
@@ -23,15 +22,6 @@ make
 make install
 
 echo "Doxygen Version:" $(doxygen --version)
-
-#sudo apt-get install libiconv # Needed? Already there? 
-
-#export VERSION=1.11.0
-#gunzip doxygen-1.11.0.src.tar.gz    # uncompress the archive
-#tar xf doxygen-1.11.0.src.tar
-
-
-
 
 
 
