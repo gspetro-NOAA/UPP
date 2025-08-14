4271616474afd2d91e2f2d12703a548e6e21fa83
Gillian Petro <gillian.petro@noaa.gov>
2025-08-14 12:23:29 -0400

    add template files

---
+++ ci/jobs-dev/run_post_3drtma_template.sh
@@ -0,0 +1,151 @@
+#!/bin/bash 
+ 
+#SBATCH -o out.post.3drtma
+#SBATCH -e out.post.3drtma
+#SBATCH -J 3drtma_test
+#SBATCH -t {{ wtime }}
+#SBATCH -q {{ queue }}
+{%- if ${machine} == "ursa" %}
+   #SBATCH --exclusive
+   #SBATCH --ntasks {{ ntasks }}
+   #SBATCH --tasks-per-node {{ tasks-per-node }}
+{%- else %}
+   #SBATCH -A {{ account }}
+   #SBATCH -N {{ nodes }} --ntasks-per-node={{ ntaskspernode }}
+{%- endif %}
+FCST_SUBDIR_TEMPLATE: '{% if user.RUN_ENVIR == "nco" %}${NET_default}.{init?fmt=%Y%m%d?shift=-${time_lag}}/{init?fmt=%H?shift=-${time_lag}}{% else %}{init?fmt=%Y%m%d%H?shift=-${time_lag}}{{ "/${ensmem_name}" if global.DO_ENSEMBLE }}/postprd{% endif %}'
+FCST_SUBDIR_TEMPLATE: 
+'
+{% if user.RUN_ENVIR == "nco" %}
+   ${NET_default}.{init?fmt=%Y%m%d?shift=-${time_lag}}/{init?fmt=%H?shift=-${time_lag}}
+{% else %}
+   {init?fmt=%Y%m%d%H?shift=-${time_lag}}{{ "/${ensmem_name}" if global.DO_ENSEMBLE }}/postprd
+{% endif %}'
+
+FCST_FN_TEMPLATE_PCPCOMBINE_OUTPUT: 
+'${NET_default}.t{init?fmt=%H}z{{ ".${ensmem_name}" 
+if user.RUN_ENVIR == "nco" and global.DO_ENSEMBLE }}.prslev.{{ task_run_post.envvars.POST_OUTPUT_DOMAIN_NAME }}.${FIELD_GROUP}${ACCUM_HH}h.{valid?fmt=%Y%m%d%H?shift=-${ACCUM_HH}H}_to_{valid?fmt=%Y%m%d%H}.nc'
+
+set -x
+
+# specify computation resources
+export MP_LABELIO=yes
+export threads=1
+export OMP_NUM_THREADS=$threads
+export APRUN="srun"
+
+echo "starting time"
+date
+
+############################################
+# Loading modules
+############################################
+module purge
+module use $svndir/modulefiles
+module load ursa_$compiler
+module load wgrib2/3.6.0
+module load prod_util/2.1.1
+module list
+
+msg="Starting 3drtma test"
+postmsg "$logfile" "$msg"
+
+export POSTGPEXEC=${svndir}/exec/upp.x
+
+# specify forecast start time and hour for running your post job
+export startdate=2023040400
+export fhr=000
+export tmmark=tm00
+
+# specify your running and output directory
+export DATA=$rundir/3drtma_${startdate}
+rm -rf $DATA; mkdir -p $DATA
+cd $DATA
+
+export NEWDATE=$startdate
+export YY=`echo ${NEWDATE} | cut -c1-4`
+export MM=`echo ${NEWDATE} | cut -c5-6`
+export DD=`echo ${NEWDATE} | cut -c7-8`
+export HH=`echo ${NEWDATE} | cut -c9-10`
+export min=00
+
+cat > itag <<EOF
+&model_inputs
+fileName='$homedir/data_in/3drtma/dynf000.nc'
+IOFORM='netcdf'
+grib='grib2'
+DateStr='${YY}-${MM}-${DD}_${HH}:${min}:00'
+MODELNAME='FV3R'
+SUBMODELNAME='RTMA'
+fileNameFlux='$homedir/data_in/3drtma/phyf${fhr}.nc'
+/
+&NAMPGB
+KPO=47,PO=1000.,975.,950.,925.,900.,875.,850.,825.,800.,775.,750.,725.,700.,675.,650.,625.,600.,575.,550.,525.,500.,475.,450.,425.,400.,375.,350.,325.,300.,275.,250.,225.,200.,175.,150.,125.,100.,70.,50.,30.,20.,10.,7.,5.,3.,2.,1.,
+/
+EOF
+
+# copy fix data
+cp ${svndir}/fix/nam_micro_lookup.dat eta_micro_lookup.dat
+cp ${svndir}/parm/params_grib2_tbl_new params_grib2_tbl_new
+cp ${svndir}/parm/rrfs/postxconfig-NT-rrfs.txt postxconfig-NT.txt
+
+# get crtm fix files
+for what in "amsre_aqua" "ssmis_f17" "ssmis_f18" \
+    "tmi_trmm" "abi_gr" "ahi_himawari8" ; do
+    ln -s "${CRTM_FIX}/${what}.TauCoeff.bin" .
+    ln -s "${CRTM_FIX}/${what}.SpcCoeff.bin" .
+done
+
+for what in 'Aerosol' 'Cloud' ; do
+    ln -s "${CRTM_FIX}/${what}Coeff.bin" .
+done
+
+for what in  ${CRTM_FIX}/*Emis* ; do
+   ln -s $what .
+done
+
+# Run the UPP
+${APRUN} ${POSTGPEXEC} < itag > outpost_3drtma_${NEWDATE}
+
+################################################
+# Compare with baseline data
+################################################
+fhr=`expr $fhr + 0`
+fhr2=`printf "%02d" $fhr`
+
+# 3DRTMA post processing generates 2 files
+filelist="NATLEV${fhr2}.${tmmark} \
+          PRSLEV${fhr2}.${tmmark}"
+
+for file in $filelist; do
+export filein2=$file
+ls -l ${filein2}
+export err=$?
+
+if [ $err = "0" ] ; then
+ # use cmp to see if new pgb files are identical to the control one
+ cmp ${filein2} $homedir/data_out_$compiler/3drtma/${filein2}.${machine}
+
+ # if not bit-identical, use cmp_grib2_grib2 to compare each grib record
+ export err1=$?
+ if [ $err1 -eq 0 ] ; then
+  msg="3drtma test: your new post executable generates bit-identical ${filein2} as the develop branch"
+  echo $msg
+ else
+  msg="3drtma test: your new post executable did not generate bit-identical ${filein2} as the develop branch"
+  echo $msg
+  echo " start comparing each grib record and write the comparison result to *diff files"
+  echo " check these *diff files to make sure your new post only change variables which you intend to change"
+  $cmp_grib2_grib2 $homedir/data_out_$compiler/3drtma/${filein2}.${machine} ${filein2} > ${filein2}.diff
+ fi
+else
+ msg="3drtma test: post failed using your new post executable to generate ${filein2}"
+ echo $msg 2>&1 | tee -a TEST_ERROR
+fi
+
+postmsg "$logfile" "$msg"
+done
+
+echo "PROGRAM IS COMPLETE!!!!!" 2>&1 | tee SUCCESS
+msg="Ending 3drtma test"
+postmsg "$logfile" "$msg"
