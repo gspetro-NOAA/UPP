702b6fc84abaa1e6949ccdeb7f15ddf026ba67de
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-06-11 04:03:34 -0700

    Unit tests for TABLE() and TABLEQ() subroutines (#1554)
    * add initial setup for table tests
    
    * array sizes
    
    * replace interfaces with module
    
    * add pt = 0.0 case to test_table
    
    * low pressure test case
    
    * implement test data file logic
    
    * value check for test case 1
    
    * change precision
    
    * Update test_table.f90
    
    * new output files
    
    * more changes to ref files
    
    * Update test_table.f90
    
    * Update test_table.f90
    
    * Update test_table.f90
    
    * Update test_table.f90
    
    * write the expected output
    
    * updated ref files
    
    * value check
    
    * Update test_table.f90
    
    * rest of value checks
    
    * trying with lower tolerance
    
    * write data for tableq tests
    
    * Update test_table.f90
    
    * fix file name
    
    * reference file for tableq tests
    
    * value check
    
    * fix some expected values
    
    * Update test_tableq.f90
    
    * tolerances
    
    * change exp value calc for rdthe due to some precision related stuff
    
    * Update test_table.f90
    
    * didnt copy test data for tableq
    
    * WCOSS2 RT log
    
    * Ursa RT log for intel compiler
    
    * Ursa RT log for intelllvm compiler
    
    * Hercules RT log for intel compiler
    
    * Orion RT log for intel compiler
    
    ---------
    
    Co-authored-by: alyson <alyson@ubuntu.myguest.virtualbox.org>
    Co-authored-by: BenjaminBlake-NOAA <52074832+BenjaminBlake-NOAA@users.noreply.github.com>
    Co-authored-by: benjamin blake <benjamin.blake@dlogin09.dogwood.wcoss2.ncep.noaa.gov>
    Co-authored-by: Chad.Lyden <Chad.Lyden@noaa.gov>

---
+++ unit_tests/test_tableq.f90
@@ -0,0 +1,125 @@
+! This is a test program for UPP.
+!
+! This program tests the TABLEQ() subroutine.
+!
+! Alyson Stahl, 6/2026
+program test_tableq
+    use tableq_upp_mod, only: TABLEQ
+    implicit none
+
+    real, parameter :: tol = 1.0e-8
+    integer, parameter :: ITB=152, JTB=440
+    integer :: i, j, res
+    !
+    character(len=*), parameter :: data_file_name = 'data/ref_tableq.txt'
+    !
+    real :: PL, THL
+    real :: TTBLQ(JTB,ITB)
+    real :: STHE(ITB), THE0(ITB)
+    real :: RDP, RDTHE
+    !
+    real :: EXP_TTBLQ(JTB,ITB), EXP_STHE(ITB), EXP_THE0(ITB)
+    real :: EXP_RDP, EXP_RDTHE
+
+    ! RDP = 1./DP -> DP = (PH - PL) / REAL(KPM - 1) = (PH - PL) / REAL(ITB - 1)
+    ! RDTHE = 1./DTHE -> DTHE = 1 / REAL(KTHM - 1) = 1 / (JTB - 1)
+
+    ! Same values used in INITPOST_GFS_NEMS_MPIIO()
+    PL = 70000.0
+    THL = 210.0
+
+    EXP_RDP = 151.0 / 35000.0
+    EXP_RDTHE = 1.0 / (1.0 / 439.0) ! Prevents precision related errors
+
+    ! Load expected data
+    call load_tableq_reference_data(data_file_name, EXP_TTBLQ, EXP_STHE, EXP_THE0)
+
+    call TABLEQ(TTBLQ, RDP, RDTHE, PL, THL, STHE, THE0)
+
+    res = 0
+
+    if (abs(RDP - EXP_RDP) > tol) then
+        print *, 'Test Failed: RDP = ', RDP, ' Expected: ', EXP_RDP
+        res = 1
+    end if
+    if (abs(RDTHE - EXP_RDTHE) > tol) then
+        print *, 'Test Failed: RDTHE = ', RDTHE, ' Expected: ', EXP_RDTHE
+        res = 1
+    end if
+    do i = 1, JTB
+        do j = 1, ITB
+            if (abs(TTBLQ(i,j) - EXP_TTBLQ(i,j)) > tol) then
+                print *, 'Test Failed: TTBLQ(', i, ',', j, ') = ', TTBLQ(i,j), &
+                         ' Expected: ', EXP_TTBLQ(i,j)
+                res = 1
+            end if
+        end do
+    end do
+
+    if (res .ne. 0) stop 10
+
+    print *, 'SUCCESS!'
+
+contains
+
+    subroutine load_tableq_reference_data(filename, ttblq_out, sthe_out, the0_out)
+        character(len=*), intent(in) :: filename
+        real, intent(out) :: ttblq_out(JTB,ITB), sthe_out(ITB), the0_out(ITB)
+        real :: temp_2d(JTB*ITB)
+        character(len=100) :: header_line
+        integer :: unit_num, i, j
+        
+        open(newunit=unit_num, file=filename, status='old', action='read')
+        
+        ! Skip 3 header lines
+        read(unit_num, '(a)') header_line
+        read(unit_num, '(a)') header_line
+        read(unit_num, '(a)') header_line
+        
+        do i = 1, JTB*ITB
+            read(unit_num, *) temp_2d(i)
+        end do
+        ttblq_out = reshape(temp_2d, [JTB, ITB])
+        
+        do i = 1, ITB
+            read(unit_num, *) sthe_out(i)
+        end do
+        
+        do i = 1, ITB
+            read(unit_num, *) the0_out(i)
+        end do
+        
+        close(unit_num)
+    end subroutine load_tableq_reference_data
+
+    subroutine write_tableq_reference_data(filename, pl_in, thl_in, ttblq_in, sthe_in, the0_in)
+        character(len=*), intent(in) :: filename
+        real, intent(in) :: pl_in, thl_in
+        real, intent(in) :: ttblq_in(JTB,ITB), sthe_in(ITB), the0_in(ITB)
+        integer :: unit_num, i, j
+        
+        open(newunit=unit_num, file=filename, status='replace', action='write')
+        
+        write(unit_num, '(a)') '# Reference data for TABLEQ() subroutine test'
+        write(unit_num, '(a,f10.1,a,f10.1)') '# Test case with PL=', pl_in, ', THL=', thl_in
+        write(unit_num, '(a,i0,a,i0,a,i0,a)') &
+            '# Format: TTBLQ(', JTB*ITB, ' values), STHE(', ITB, '), THE0(', ITB, ')'
+        
+        do i = 1, ITB
+            do j = 1, JTB
+                write(unit_num, '(es24.16)') ttblq_in(j,i)
+            end do
+        end do
+        
+        do i = 1, ITB
+            write(unit_num, '(es24.16)') sthe_in(i)
+        end do
+        
+        do i = 1, ITB
+            write(unit_num, '(es24.16)') the0_in(i)
+        end do
+        
+        close(unit_num)
+    end subroutine write_tableq_reference_data
+    
+end program test_tableq
